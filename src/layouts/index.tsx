6dfd8c69ea5c067f2e4e5863ebf7af316fc986d8
sammy.uyghur <sammy.uyghur@ucloud.cn>
2023-01-07 16:13:37 +0800

    feat: add background

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,6 +3,7 @@ import React, { FunctionComponent, ReactElement } from "react";
 import { navigate } from "gatsby"
 import { Menu, Icon, Box, Button } from "miever_ui";
 import { useLocation } from '@reach/router';
+import ParticlesContainer from "./particles-container";
 
 import "miever_ui/dist/index.css"
  
@@ -14,86 +15,89 @@ const Layout: FunctionComponent<{
     const defaultKey = pathname === "/" ? "home" : pathname.split("/")[1];
 
     return (
-        <Box
-            flexBox
-            direction="column"
-            style={{ minHeight: "100vh" }}
-            justifyContent="space-between"
-        >
-            <Box padding="4px 0 0 0">
-                <Menu
-                    defaultKey={defaultKey}
-                    style={{
-                        minWidth: "1200px"
-                    }}
-                    prefix={(
-                        <Box
-                            width="240px"
-                            style={{
-                                backgroundImage: "url(/home.png)",
-                                backgroundPosition: "24px",
-                                backgroundSize: "180px",
-                                backgroundRepeat: "no-repeat",
-                                cursor: "pointer"
-                            }}
-                            onClick={() => window.location.replace("/")}
-                        />
-                    )}
-                    suffix={(
-                      <Box flexBox alignItems="center" style={{ paddingLeft: "500px" }}>
-                        <Button
-                            styleType="link"
-                            onClick={() => window.open("https://github.com/Miever1")}
-                        >
-                            <Icon icon={["fab", "github"]} theme="primary" style={{ fontSize: "14px", cursor: "pointer" }}/>
-                        </Button>
-                      </Box>
-                    )}
-                    items={[
-                        {
-                            label: "Home",
-                            key: "home"
-                        },
-                        {
-                            label: "Dashboard",
-                            key: "dashboard"
-                        },
-                        {
-                            label: "Blogs",
-                            key: "blogs"
-                        },
-                        {
-                            label: "Resume",
-                            key: "resume"
-                        },
-                    ]}
-                    onSelect={(value: string) => {
-                        navigate(value === "home" ? "/" : `/${value}`)
-                    }}
-                />
-            </Box>
-            <Box padding={pathname === "/dashboard/" ? "" : "0 290px"} style={{ minWidth: "1200px", height: `${pathname === "/dashboard/" ? "calc(100vh - 80px)" : ""}` }}>
-                {children}
-            </Box>
+        <Box>
+            <ParticlesContainer />
             <Box
                 flexBox
-                height={120}
                 direction="column"
-                justifyContent="center"
-                alignItems="center"
-                paddingY={8}
-                style={{ minWidth: "1200px" }}
+                style={{ minHeight: "100vh" }}
+                justifyContent="space-between"
             >
-                <Button
-                    styleType="link"
-                    onClick={() => window.open("https://components.miever.net")}
-                >
-                    The website is designed by miever_ui!
-                </Button>
+                <Box style={{ background: "#FFF", width: "100%" }}>
+                    <Menu
+                        defaultKey={defaultKey}
+                        style={{
+                            minWidth: "1200px"
+                        }}
+                        prefix={(
+                            <Box
+                                width="240px"
+                                style={{
+                                    backgroundImage: "url(/home.png)",
+                                    backgroundPosition: "24px",
+                                    backgroundSize: "180px",
+                                    backgroundRepeat: "no-repeat",
+                                    cursor: "pointer"
+                                }}
+                                onClick={() => window.location.replace("/")}
+                            />
+                        )}
+                        suffix={(
+                        <Box flexBox alignItems="center" style={{ paddingLeft: "500px" }}>
+                            <Button
+                                styleType="link"
+                                onClick={() => window.open("https://github.com/Miever1")}
+                            >
+                                <Icon icon={["fab", "github"]} theme="primary" style={{ fontSize: "14px", cursor: "pointer" }}/>
+                            </Button>
+                        </Box>
+                        )}
+                        items={[
+                            {
+                                label: "Home",
+                                key: "home"
+                            },
+                            {
+                                label: "Dashboard",
+                                key: "dashboard"
+                            },
+                            {
+                                label: "Blogs",
+                                key: "blogs"
+                            },
+                            {
+                                label: "Resume",
+                                key: "resume"
+                            },
+                        ]}
+                        onSelect={(value: string) => {
+                            navigate(value === "home" ? "/" : `/${value}`)
+                        }}
+                    />
+                </Box>
+                <Box padding={pathname === "/dashboard/" ? "" : "0 290px"} style={{ minWidth: "1200px", height: `${pathname === "/dashboard/" ? "calc(100vh - 80px)" : ""}` }}>
+                    {children}
+                </Box>
                 <Box
-                    style={{ fontSize: "12px" }}
+                    flexBox
+                    height={120}
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    paddingY={8}
+                    style={{ minWidth: "1200px" }}
                 >
-                    Copyright © 2022-2023 Miever
+                    <Button
+                        styleType="link"
+                        onClick={() => window.open("https://components.miever.net")}
+                    >
+                        The website is designed by miever_ui!
+                    </Button>
+                    <Box
+                        style={{ fontSize: "12px" }}
+                    >
+                        Copyright © 2022-2023 Miever
+                    </Box>
                 </Box>
             </Box>
         </Box>
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Dashboard() {
     return (
-        <iframe width="100%" height="100%" src="https://p.us5.datadoghq.com/sb/071388bc-8801-11ed-9d34-da7ad0900005-e2ef47e4eb064ee8bd7652bc58a9a0f5?tpl_var_applicationId%5B0%5D=%2A&tpl_var_browser%5B0%5D=%2A&tpl_var_country%5B0%5D=%2A&tpl_var_device%5B0%5D=%2A&tpl_var_env%5B0%5D=%2A&tpl_var_OS%5B0%5D=%2A&tpl_var_service%5B0%5D=%2A&tpl_var_sessionType%5B0%5D=user&tpl_var_version%5B0%5D=%2A&from_ts=1664948270349&to_ts=1672724270349&live=true" />
+        <iframe width="100%" style={{ opacity: ".9"}} height="100%" src="https://p.us5.datadoghq.com/sb/071388bc-8801-11ed-9d34-da7ad0900005-e2ef47e4eb064ee8bd7652bc58a9a0f5?tpl_var_applicationId%5B0%5D=%2A&tpl_var_browser%5B0%5D=%2A&tpl_var_country%5B0%5D=%2A&tpl_var_device%5B0%5D=%2A&tpl_var_env%5B0%5D=%2A&tpl_var_OS%5B0%5D=%2A&tpl_var_service%5B0%5D=%2A&tpl_var_sessionType%5B0%5D=user&tpl_var_version%5B0%5D=%2A&from_ts=1664948270349&to_ts=1672724270349&live=true" />
     )
 }
