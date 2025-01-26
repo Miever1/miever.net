a48638e6b7f4fbbfaa9d4f5c0f34c2c66869b66f
miever <miever1@163.com>
2025-01-26 12:42:57 +0100

    fix: resume viewer bug fixed

--- src/pages/resume/resume.tsx
+++ src/pages/resume/resume.tsx
@@ -1,8 +1,8 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import { Box, designs } from "miever_ui";
 import { Spinner } from "@chakra-ui/react";
 
-const Resume:FunctionComponent<{}> = () => {
+const Resume: FunctionComponent<{}> = () => {
     const { BRAND_COLORS } = designs;
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -11,8 +11,18 @@ const Resume:FunctionComponent<{}> = () => {
         console.log("Iframe has finished loading!");
     };
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (!isLoaded) {
+                console.warn("Iframe loading timeout, removing spinner.");
+                setIsLoaded(true);
+            }
+        }, 5000);
+
+        return () => clearTimeout(timeout);
+    }, [isLoaded]);
+
     return (
-        
         <Box style={{ width: "100%", height: "100%", position: "relative" }}>
             {!isLoaded && (
                 <Box
@@ -27,17 +37,19 @@ const Resume:FunctionComponent<{}> = () => {
                     <Spinner size="xl" color={BRAND_COLORS.primary} />
                 </Box>
             )}
-            <embed
-                src="https://miever.s3.ap-east-1.amazonaws.com/static/resume/miever.pdf"
-                type="application/pdf"
+            <iframe
+                src="https://docs.google.com/viewer?url=https://miever.s3.ap-east-1.amazonaws.com/static/resume/miever.pdf&embedded=true"
                 style={{
                     height: "100vh",
                     width: "100%",
+                    border: "none",
                 }}
                 onLoad={handleLoad}
+                onError={() => console.error("Iframe failed to load")}
+                title="PDF Viewer"
             />
         </Box>
-    )
-}
+    );
+};
 
 export default Resume;
